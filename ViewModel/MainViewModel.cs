0edcce5f8588e2bc44cb75ea09afb254ac97cda6
HP <HP@LAPTOP-60G5O5GF>
2022-04-17 13:28:59 +0700

    Add project files.

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Nhom13_Quan_ly_kho_hang.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            MessageBox.Show("Đã vào trong main");
+        }
+    }
+}
