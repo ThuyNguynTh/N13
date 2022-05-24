aac02e49d0d872b6f44d8b85989cc8368fb6ed78
Cường Lê <Lemanhcuong240201@outlook.com.vn>
2022-05-25 00:29:21 +0700

    cap nhat them sua unit

--- Model/Unit.cs
+++ Model/Unit.cs
@@ -9,10 +9,11 @@
 
 namespace Nhom13_Quan_ly_kho_hang.Model
 {
+    using Nhom13_Quan_ly_kho_hang.ViewModel;
     using System;
     using System.Collections.Generic;
     
-    public partial class Unit
+    public partial class Unit : BaseViewModel
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Unit()
@@ -21,7 +22,9 @@ namespace Nhom13_Quan_ly_kho_hang.Model
         }
     
         public int Id { get; set; }
-        public string DisplayName { get; set; }
+
+        private string _DisplayName;
+        public string DisplayName { get => _DisplayName; set { _DisplayName = value; OnPropertyChanged(); } }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Object> Objects { get; set; }
